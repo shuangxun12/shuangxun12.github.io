92154fe9420a98b28f24805f54b0083f56e57228
shuangxun12 <144909515+shuangxun12@users.noreply.github.com>
2024-02-02 17:07:48 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,19 +1,1 @@
 alert("false");
-var d = new Date();
-alert("right");
-document.write("<br>");
-document.write("现在的时间是：");
-document.write(d.getFullYear());
-document.write("年")；
-document.write(d.getMonth()+1);
-document.write("月");
-document.write(d.getDate());
-document.write("日");
-document.write(d.getHours());
-document.write("点")；
-document.write(d.getMinutes());
-document.write("分");
-document.write(d.getSeconds());
-document.write("秒")；
-document.write("<br>");
-document.write("<br>");
