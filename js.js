8ec331f1009b5c71e5288a3a3b4f6a2fd8f1af1a
shuangxun12 <144909515+shuangxun12@users.noreply.github.com>
2024-02-02 17:12:25 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,17 +1,19 @@
-var d = new Date();
-document.write("<br>");
-document.write("现在的时间是：");
-document.write(d.getFullYear());
-document.write("年")；
-document.write(d.getMonth()+1);
-document.write("月");
-document.write(d.getDate());
-document.write("日");
-document.write(d.getHours());
-document.write("点")；
-document.write(d.getMinutes());
-document.write("分");
-document.write(d.getSeconds());
-document.write("秒")；
-document.write("<br>");
-document.write("<br>");
+function fun(){
+  var d = new Date();
+  document.write("<br>");
+  document.write("现在的时间是：");
+  document.write(d.getFullYear());
+  document.write("年")；
+  document.write(d.getMonth()+1);
+  document.write("月");
+  document.write(d.getDate());
+  document.write("日");
+  document.write(d.getHours());
+  document.write("点")；
+  document.write(d.getMinutes());
+  document.write("分");
+  document.write(d.getSeconds());
+  document.write("秒")；
+  document.write("<br>");
+  document.write("<br>");
+}
