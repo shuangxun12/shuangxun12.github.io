4ec6e074bf42d0e7296e7aff41c72a7c31d4535f
shuangxun12 <144909515+shuangxun12@users.noreply.github.com>
2024-02-02 16:50:12 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,11 +1,17 @@
-function url() {
-	alert("网页标题： "+document.title);
-
-	alert("网页地址: "+document.URL);
-
-	var value = document.getElementById("mylabel").innerHTML;
-	alert(value);
-	alert("hello");
-	document.getElementById("mylabel").innerHTML = "hello world!";
-}
-
+var d = new Date();
+document.write("<br>");
+document.write("现在的时间是：");
+document.write(d.getFullYear());
+document.write("年")；
+document.write(d.getMonth()+1);
+document.write("月");
+document.write(d.getDate());
+document.write("日");
+document.write(d.getHours());
+document.write("点")；
+document.write(d.getMinutes());
+document.write("分");
+document.write(d.getSeconds());
+document.write("秒")；
+document.write("<br>");
+document.write("<br>");
