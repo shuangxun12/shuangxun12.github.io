20be5c9b3a0cdded23b6ee7aa1eb1f7e9591d89f
shuangxun12 <144909515+shuangxun12@users.noreply.github.com>
2024-02-03 09:27:03 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 function mymath(arg){
-  var num1 = document.getElementsById("num1");
-  var num2 = document.getElementsById("num2");
+  var num1 = document.getElementById("num1");
+  var num2 = document.getElementById("num2");
   if(isNaN(num1) || isNaN(num2))
   {
   	alert("请输入数字");
@@ -10,16 +10,16 @@ function mymath(arg){
   	swich(arg)
   	{
   		case 1:
-  			document.getElementsById("ret") = Math.max(num1, num2);
+  			document.getElementById("ret") = Math.max(num1, num2);
   			break;
   		case 2:
-  			document.getElementsById("ret") = Math.min(num1, num2);
+  			document.getElementById("ret") = Math.min(num1, num2);
   			break;
   		case 3:
-  			document.getElementsById("ret") = Number(num1) + Number(num2);
+  			document.getElementById("ret") = Number(num1) + Number(num2);
   			break;
   		case 4:
-  			document.getElementsById("ret") = Number(num1) - Number(num2);
+  			document.getElementById("ret") = Number(num1) - Number(num2);
   			break;
   	}
   }
