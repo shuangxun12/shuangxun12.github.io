ca6b6cd54787827fe37ea79b691778865b726a96
shuangxun12 <144909515+shuangxun12@users.noreply.github.com>
2024-02-03 09:51:50 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,26 +1,27 @@
 function mymath(arg) {
-	var num1 = document.getElementById("num1").value;
-	var num2 = document.getElementById("num2").value;
-	if(isNaN(num1) || isNaN(num2))
-	{
-		alert("请输入数字");
-	}
-	else
-	{
-		switch(arg)
-		{
-			case 1:
-				document.getElementById("ret").value = Math.max(num1, num2);
-			break;
-			case 2:
-				document.getElementById("ret").value = Math.min(num1, num2);
-			break;
-			case 3:
-				document.getElementById("ret").value = Number(num1) + Number(num2);
-			break;
-			case 4:
-				document.getElementById("ret").value = Number(num1) - Number(num2);
-			break;
-		}
-	}
+	// var num1 = document.getElementById("num1").value;
+	// var num2 = document.getElementById("num2").value;
+	// if(isNaN(num1) || isNaN(num2))
+	// {
+	// 	alert("请输入数字");
+	// }
+	// else
+	// {
+	// 	switch(arg)
+	// 	{
+	// 		case 1:
+	// 			document.getElementById("ret").value = Math.max(num1, num2);
+	// 		break;
+	// 		case 2:
+	// 			document.getElementById("ret").value = Math.min(num1, num2);
+	// 		break;
+	// 		case 3:
+	// 			document.getElementById("ret").value = Number(num1) + Number(num2);
+	// 		break;
+	// 		case 4:
+	// 			document.getElementById("ret").value = Number(num1) - Number(num2);
+	// 		break;
+	// 	}
+	// }
+	alert("你好");
 }
